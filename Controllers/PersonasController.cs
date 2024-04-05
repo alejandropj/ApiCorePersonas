c99b2a5c6e7e4d58168727606a739f5eeae9ff74
alejandropj <alejandroparrajimenez@gmail.com>
2024-04-05 10:05:08 +0200

    Add project files.

---
+++ Controllers/PersonasController.cs
@@ -0,0 +1,61 @@
+﻿using ApiCorePersonas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCorePersonas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        List<Persona> personas;
+
+        public PersonasController()
+        {
+            this.personas = new List<Persona>();
+            Persona p = new Persona
+            {
+                IdPersona = 1,
+                Nombre = "Alumno",
+                Email = "alumno@mail.com",
+                Edad = 22
+            };
+            this.personas.Add(p);
+            p = new Persona
+            {
+                IdPersona = 1,
+                Nombre = "Lucia",
+                Email = "alumnoo@mail.com",
+                Edad = 22
+            };
+            this.personas.Add(p);
+            p = new Persona
+            {
+                IdPersona = 1,
+                Nombre = "Annie",
+                Email = "annie@mail.com",
+                Edad = 28
+            };
+            this.personas.Add(p);
+            p = new Persona
+            {
+                IdPersona = 1,
+                Nombre = "Pedro",
+                Email = "pedrito@mail.com",
+                Edad = 89
+            };
+            this.personas.Add(p);
+        }
+
+        [HttpGet]
+        public ActionResult<List<Persona>> Get()
+        {
+            return this.personas;
+        }        
+        [HttpGet]
+        public ActionResult<Persona> FindPersona(int id)
+        {
+            return this.personas.FirstOrDefault(x=>x.IdPersona == id);
+        }
+    }
+}
